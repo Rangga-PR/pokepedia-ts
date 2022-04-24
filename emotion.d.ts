3d94636b8a55aafea5f910bbb8f0e59ba6c0a6b2
Rangga-PR <ranggaputraraharja@gmail.com>
2022-04-24 14:07:53 +0700

    setup theme and emotion

---
+++ emotion.d.ts
@@ -0,0 +1,28 @@
+import "@emotion/react";
+
+declare module "@emotion/react" {
+  export interface Theme {
+    color: {
+      red: string;
+      gray: string;
+      normal: string;
+      fire: string;
+      water: string;
+      electric: string;
+      grass: string;
+      ice: string;
+      fighting: string;
+      poison: string;
+      ground: string;
+      flying: string;
+      psychic: string;
+      bug: string;
+      rock: string;
+      ghost: string;
+      dragon: string;
+      dark: string;
+      steel: string;
+      fairy: string;
+    };
+  }
+}
